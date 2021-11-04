1f26c06c852b2e89666dfe9a9575a22949234633
John <lijiang01001@163.com>
2021-11-04 21:56:06 +0800

    vector2

--- src/hsm.hpp
+++ src/hsm.hpp
@@ -58,11 +58,11 @@ public:
 		return *this;
 	}
 
-	bool operator == (const Point2<T> p) const {
+	bool operator == (const Point2<T>& p) const {
 		return x == p.x && y == p.y;
 	}
 
-	bool operator != (const Point2<T> p) const {
+	bool operator != (const Point2<T>& p) const {
 		return x != p.x || y != p.y;
 	}
 
@@ -84,6 +84,10 @@ public:
 
 	Point2<T> operator + (const Vector2<T>& v) const {
 		return Point2<T>(x + v.x, y + v.y);
+	}
+
+	Point2<T> operator + (const Point2<T>& p) const {
+		return Point2<T>(x + p.x, y + p.y);
 	}
 
 	Point2<T>& operator += (const Vector2<T>& v) {
@@ -134,6 +138,17 @@ public:
 		return *this;
 	}
 
+	Float distanceSquared(const Point2<T>& p) const { 
+		return std::pow(x - p.x, 2) + std::pow(y - p.y, 2);
+	}
+	Float distance(const Point2<T>& p) const { return std::sqrt(distanceSquared(p)); }
+
+	Point2<T> floor() const { return Point2<T>(std::floor(x), std::floor(y)); }
+	Point2<T> ceil() const { return Point2<T>(std::ceil(x), std::ceil(y)); }
+	Point2<T> abs() const { return Point2<T>(std::abs(x), std::abs(y)); }
+
+	Point2<T> lerp(const Point2<T>& p, Float t) const { return (1 - t)*(*this) + t * p; }
+
 	//public data
 	T x, y;
 };
@@ -142,6 +157,11 @@ template<typename T>
 std::ostream & operator << (std::ostream &o, const Point2<T>& p) {
 	o << '[' << p.x << ',' << p.y << ']';
 	return o;
+}
+
+template<typename T, typename U>
+inline Point2<T> operator * (U n, const Point2<T>& v) {
+	return v * n;
 }
 
 template<typename T> class Vector3;
@@ -178,11 +198,11 @@ public:
 		return *this;
 	}
 
-	bool operator == (const Point3<T> p) const {
+	bool operator == (const Point3<T>& p) const {
 		return x == p.x && y == p.y && z == p.z;
 	}
 
-	bool operator != (const Point3<T> p) const {
+	bool operator != (const Point3<T>& p) const {
 		return x != p.x || y != p.y || z != p.z;
 	}
 
@@ -198,7 +218,7 @@ public:
 	}
 
 	T& operator [](int i) {
-		assert(i >= 0 && i <= 3);
+		assert(i >= 0 && i <= 2);
 		if (i == 0) return x;
 		else if (i == 1) return y;
 		return z;
@@ -206,6 +226,10 @@ public:
 
 	Point3<T> operator + (const Vector3<T>& v) const {
 		return Point3<T>(x + v.x, y + v.y, z + v.z);
+	}
+
+	Point3<T> operator + (const Point3<T>& p) const {
+		return Point3<T>(x + p.x, y + p.y, z + p.z);
 	}
 
 	Point3<T>& operator += (const Vector3<T>& v) {
@@ -260,6 +284,17 @@ public:
 		return *this;
 	}
 
+	Float distanceSquared(const Point3<T>& p) const {
+		return std::pow(x - p.x, 2) + std::pow(y - p.y, 2) + std::pow(z - p.z, 2);
+	}
+	Float distance(const Point3<T>& p) const { return std::sqrt(distanceSquared(p)); }
+
+	Point3<T> floor() const { return Point3<T>(std::floor(x), std::floor(y), std::floor(z)); }
+	Point3<T> ceil() const { return Point3<T>(std::ceil(x), std::ceil(y), std::ceil(z)); }
+	Point3<T> abs() const { return Point3<T>(std::abs(x), std::abs(y), std::abs(z)); }
+
+	Point3<T> lerp(const Point3<T>& p, Float t) const { return (1 - t)*(*this) + t * p; }
+
 	//public data
 	T x, y, z;
 };
@@ -270,6 +305,11 @@ std::ostream & operator << (std::ostream &o, const Point3<T>& p) {
 	return o;
 }
 
+template<typename T, typename U>
+inline Point3<T> operator * (U n, const Point3<T>& v) {
+	return v * n;
+}
+
 //two-dimensional vector
 template<typename T>
 class Vector2 {
@@ -278,9 +318,123 @@ public:
 	Vector2() :x(0), y(0) {}
 	Vector2(T xx, T yy) :x(xx), y(yy) {}
 
+	template<typename S>
+	explicit Vector2(const Point2<T>& p) {
+		x = (T)p.x;
+		y = (T)p.y;
+	}
+
+	template<typename S>
+	explicit Vector2(const Vector2<T>& v) {
+		x = (T)v.x;
+		y = (T)v.y;
+	}
+
+	bool hasNaN() const { return isNaN(x) || isNaN(y); }
+
+	Vector2<T>& operator = (const Vector2<T>& v) {
+		x = v.x;
+		y = v.y;
+		return *this;
+	}
+
+	bool operator == (const Vector2<T>& v) const {
+		return x == v.x && y == v.y;
+	}
+
+	bool operator != (const Vector2<T>& v) const {
+		return x != v.x || y != v.y;
+	}
+
+	Vector2<T> operator -() const {
+		return Vector2<T>(-x, -y);
+	}
+
+	T operator [](int i) const {
+		assert(i >= 0 && i <= 1);
+		if (i == 0) return x;
+		return y;
+	}
+
+	T& operator [](int i) {
+		assert(i >= 0 && i <= 1);
+		if (i == 0) return x;
+		return y;
+	}
+
+	Vector2<T> operator + (const Vector2<T>& v) const {
+		return Point3<T>(x + v.x, y + v.y);
+	}
+
+	Vector2<T>& operator += (const Vector2<T>& v) {
+		x += v.x;
+		y += v.y;
+		return *this;
+	}
+
+	Vector2<T> operator - (const Vector2<T>& v) const {
+		return Point3<T>(x - v.x, y - v.y);
+	}
+
+	Vector2<T>& operator -= (const Vector2<T>& v) {
+		x -= v.x;
+		y -= v.y;
+		return *this;
+	}
+
+	template<typename S>
+	Vector2<T> operator *(S n) const {
+		return Vector2<T>(n * x, n * y);
+	}
+
+	template<typename S>
+	Vector2<T>& operator *= (S n) {
+		x *= n;
+		y *= n;
+		return *this;
+	}
+
+	template<typename S>
+	Vector2<T> operator / (S n) const {
+		assert(n != 0);
+		Float inverse = (Float)1 / n;
+		return Vector2<T>(x * inverse, y * inverse);
+	}
+
+	template<typename S>
+	Vector2<T>& operator /= (S n) {
+		assert(n != 0);
+		Float inverse = (Float)1 / n;
+		x *= inverse;
+		y *= inverse;
+		return *this;
+	}
+
+	Float lengthSquared()const { return x * x + y * y; }
+	Float length()const { return std::sqrt(lengthSquared()); }
+
+	Vector2<T> abs() const { return Vector2<T>(std::abs(x), std::abs(y)); }
+	Vector2<T> normalize() const { return *this / length(); }
+
 	//public data
 	T x, y;
 };
+
+template<typename T>
+inline std::ostream& operator << (std::ostream& o, const Vector2<T>& v) {
+	o << '[' << v.x << ',' << v.y << ']';
+	return o;
+}
+
+template<typename T, typename U>
+inline Vector2<T> operator * (U n, const Vector2<T>& v) {
+	return v * n;
+}
+
+template<typename T>
+inline Float dot(const Vector2<T>& v1, const Vector2<T>& v2) {
+	return v1.x * v2.x + v1.y * v2.y;
+}
 
 //three-dimensional vector
 template<typename T>
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,17 @@
 #include "iostream"
 int main() {
 	hsm::Point3<int> p1;
-	hsm::Point3<float> p2(0.003, -0.005, -6);
-	hsm::Point3<float> p3 = hsm::Point3<float>(1.58, 9.96, 0.05f);
-	hsm::Point3<float> p4 = hsm::Point3<float>(1.58, 9.96, 0.05f);
+	hsm::Point3<float> p2(0.003, -0.005, 9.3f);
+	hsm::Point3<float> p3 = hsm::Point3<float>(1.58, 9.96, -99.2f);
+	hsm::Point3<float> p4 = hsm::Point3<float>(1.58, 9.96, -99.2f);
 	std::cout << p1 << std::endl;
 	std::cout << p2 << std::endl;
 	std::cout << p3 << std::endl;
+	std::cout << p3.distance(p2) << std::endl;
+	std::cout << p3.floor() << std::endl;
+	std::cout << p3.ceil() << std::endl;
+	std::cout << p2.abs() << std::endl;
+	std::cout << p2.lerp(p3,0.5f) << std::endl;
 	std::cout << p3*5 << std::endl;
 	std::cout << p3*1.23 << std::endl;
 	std::cout << p3*0 << std::endl;
@@ -16,12 +21,18 @@ int main() {
 	std::cout << p3/5.24 << std::endl;
 	std::cout << p3[0] << std::endl;
 	std::cout << p3[1] << std::endl;
-	std::cout << p3[2] << std::endl;
 	std::cout << -p3 << std::endl;
 	std::cout << (p3-p2).x << std::endl;
 	std::cout << (p3-p2).y << std::endl;
 	if(p3 == p4)std::cout << "1" << std::endl;
 	if(p3 != p2)std::cout << "2" << std::endl;
+
+	hsm::Vector2<float>v1(0.0036f, -96.5f);
+	hsm::Vector2<float>v2(1.5f, 9.6f);
+	std::cout << v1.length() << std::endl;
+	std::cout << v1.abs() << std::endl;
+	std::cout << v1.normalize() << std::endl;
+	std::cout << dot(v1,v2) << std::endl;
 	system("pause");
 	return 0;
 }
